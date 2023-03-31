43a8df7b0ebf723bc7a15c9ee1138e42827e52e1
Graham Darcey <graham@hackclub.com>
2023-03-31 14:19:30 -0400

    Added basic http authentication wrapping db-hitting endpoints

--- src/airtable.ts
+++ src/airtable.ts
@@ -1,4 +1,3 @@
-import { exec } from 'child_process';
 import puppeteer, { Page, TimeoutError } from 'puppeteer';
 import { base } from "./config.js";
 
@@ -166,6 +165,15 @@ export async function generateLevel() {
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
+
+  page
+  .on('console', message =>
+    console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
+  .on('pageerror', ({ message }) => console.log(message))
+  .on('response', response =>
+    console.log(`${response.status()} ${response.url()}`))
+  .on('requestfailed', request =>
+    console.log(`${request.failure().errorText} ${request.url()}`))
 
   await page.setViewport({ width: 1280, height: 720 });
 
